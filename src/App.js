d3bcefa7be6e3178f5cb2f11236a6019b335b5f1
Crystickal <127527741+Crystickal@users.noreply.github.com>
2023-04-11 15:49:01 -0700

    brought over tetris

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<p><div class="multiplayer-box"><a href="/multiplayer">
+            <h1 style={{color:"pink"}}>MUTIPLAYER</h1>
+            <h2 style={{color:"pink"}}>Play online with your friends</h2></a>
+        </div>
+        <a href="https://www.youtube.com" target="_blank" rel="noreferrer"><div class="solo" href="#">
+            <h1 style={{color:"darkblue"}}>SOLO</h1>
+            <h2 style={{color:"darkblue"}}>Challenge yourself and top the leaderboard</h2>
+        </div></a>
+       <a href="https://www.youtube.com" target="_blank"  rel="noreferrer"><div class="tetra" href="#">
+            <h1 style={{color:"green"}}>TETRA CHANNEL</h1>
+            <h2 style={{color:"green"}}>Leaderboard, replays and more</h2>
+       </div></a>
+        <a><div class="config" href="#">
+            <h1 style={{color:"blueviolet"}}>CONFIG</h1>
+            <h2 style={{color:"blueviolet"}}>Tweak your Tetr.io experience</h2>
+        </div></a>
+        <div class="about" href="#">
+            <h1 style={{color:"grey"}}>ABOUT</h1>
+            <h2 style={{color:"grey"}}>All about Tetr.io</h2>
+        </div></p>}></Route>
+        <Route path="/something" element={<p>Hello</p>}></Route>
+        <Route path="/multiplayer" element={<div>
+            <div> <a href="/">
+                <h1>back</h1>
+            </a></div>
+            <div class="multiplayer-box">
+            <h1 style={{color:"red"}}>TETRA LEAGUE</h1>
+            <h2 style={{color:"red"}}>Fight players of your skills in ranked duels</h2></div></div>}></Route>
+    </Routes>
+</BrowserRouter>
+        {/* <div class="multiplayer-box" href="#">
+            <h1 style={{color:"pink"}}>MUTIPLAYER</h1>
+            <h2 style={{color:"pink"}}>Play online with your friends</h2>
+        </div>
+        <a href="https://www.youtube.com" target="_blank"><div class="solo" href="#">
+            <h1 style={{color:"darkblue"}}>SOLO</h1>
+            <h2 style={{color:"darkblue"}}>Challenge yourself and top the leaderboard</h2>
+        </div></a>
+       <a href="https://www.youtube.com" target="_blank"><div class="tetra" href="#">
+            <h1 style={{color:"green"}}>TETRA CHANNEL</h1>
+            <h2 style={{color:"green"}}>Leaderboard, replays and more</h2>
+       </div></a>
+        <a><div class="config" href="#">
+            <h1 style={{color:"blueviolet"}}>CONFIG</h1>
+            <h2 style={{color:"blueviolet"}}>Tweak your Tetr.io experience</h2>
+        </div></a>
+        <div class="about" href="#">
+            <h1 style={{color:"grey"}}>ABOUT</h1>
+            <h2 style={{color:"grey"}}>All about Tetr.io</h2>
+        </div> */}
     </div>
   );
 }
